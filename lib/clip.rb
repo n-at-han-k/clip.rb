3153b2f60fa7a80697b62ce68f0da3544551053f
nathan <nathankidd@hey.com>
2026-01-15 21:24:53 +0000

    initial ruby gem

---
+++ lib/clip.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "clip/version"
+require_relative "clip/cli"
+
+module Clip
+end
---
+++ lib/clip/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clip
+  VERSION = "1.0.0"
+end
